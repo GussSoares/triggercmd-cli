a611f77d8262cf8c1c5a99cd3039237e52133564
GussSoares <gustavo.soares.cdc@gmail.com>
2021-09-20 11:54:40 -0300

    [feature] add to PyPi

---
+++ setup.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+setup(
+    name="triggercmd-cli",
+    version="0.1.0",
+    url="https://github.com/GussSoares/triggercmd-cli",
+    license="MIT License",
+    author="Gustavo Soares",
+    author_email="gustavo.soares.cdc@gmail.com",
+    keywords="triggercmd alexa echo-dot cli archlinux manjaro",
+    description=u"CLI to TriggerCMD to archlinux distribuction based.",
+    packages=["triggercmd_cli"],
+    install_requires=[],
+)

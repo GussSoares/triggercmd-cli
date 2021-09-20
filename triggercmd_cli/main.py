29c5d242b83d25aca83adbf19aedc78aa9c57390
GussSoares <gustavo.soares.cdc@gmail.com>
2021-09-20 11:32:44 -0300

    [feature] add -v flag to show current version

--- triggercmd_cli/main.py
+++ triggercmd_cli/main.py
@@ -1,10 +1,31 @@
 import typer
 
+from triggercmd_cli import __version__
 from triggercmd_cli.command.command import command_app
 
 app = typer.Typer(help=__doc__)
 
 app.add_typer(command_app, name="command")
+
+
+def get_version(version: bool):
+    if version:
+        typer.echo(f"\nTriggerCMD CLI Version: {__version__}\n")
+        raise typer.Exit()
+
+
+@app.callback()
+def main(
+    version: bool = typer.Option(
+        None,
+        "--version",
+        "-v",
+        callback=get_version,
+        is_eager=True,
+        help="Display TriggerCMD CLI version.",
+    )
+):
+    return
 
 
 def run():
